92997afc1685363c86fbe97a28097762145a3175
Arin Artoun <76731903+arinartoun@users.noreply.github.com>
2026-06-26 15:21:24 +0330

    Update QuizContext.tsx

--- src/contexts/QuizContext.tsx
+++ src/contexts/QuizContext.tsx
@@ -130,7 +130,7 @@ function QuizProvider({ children }: QuizProviderProps) {
     : 0;
 
   useEffect(function () {
-    fetch("http://localhost:3000/questions")
+    fetch("http://localhost:9000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
